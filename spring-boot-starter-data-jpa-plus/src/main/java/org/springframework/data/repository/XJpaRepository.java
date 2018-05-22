24a379cd405e7a8e4841bdb6c3d01530f2de3327
SPT Developer <spt.developer@gmail.com>
2018-05-22 16:56:14 +0900

    Update headers

--- spring-boot-starter-data-jpa-plus/src/main/java/org/springframework/data/repository/XJpaRepository.java
+++ spring-boot-starter-data-jpa-plus/src/main/java/org/springframework/data/repository/XJpaRepository.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.springframework.data.repository;
 
 import java.io.Serializable;
